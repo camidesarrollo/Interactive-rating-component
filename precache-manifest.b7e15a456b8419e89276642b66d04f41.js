4a4b14e0a7108298f00da16a43730a31bbfce587
camidesarrollo <101568254+camidesarrollo@users.noreply.github.com>
2022-06-04 00:47:11 -0400

    Updates

---
+++ precache-manifest.b7e15a456b8419e89276642b66d04f41.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "85e27f5a6e83785df9f4",
+    "url": "/camidesarrollo/Interactive-rating-component.github.io/static/css/main.2449ed87.chunk.css"
+  },
+  {
+    "revision": "85e27f5a6e83785df9f4",
+    "url": "/camidesarrollo/Interactive-rating-component.github.io/static/js/main.85e27f5a.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/camidesarrollo/Interactive-rating-component.github.io/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "a3c2df52f68232dedd8f",
+    "url": "/camidesarrollo/Interactive-rating-component.github.io/static/js/2.a3c2df52.chunk.js"
+  },
+  {
+    "revision": "ad62a833666a75c1e683",
+    "url": "/camidesarrollo/Interactive-rating-component.github.io/static/js/runtime~main.ad62a833.js"
+  },
+  {
+    "revision": "e1ac14204e8aba3596472a748fa7fc26",
+    "url": "/camidesarrollo/Interactive-rating-component.github.io/static/media/iconstar.e1ac1420.svg"
+  },
+  {
+    "revision": "cb8e07d4b2e2030ca91a82f5aae61dcd",
+    "url": "/camidesarrollo/Interactive-rating-component.github.io/static/media/illustrationthankyou.cb8e07d4.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/camidesarrollo/Interactive-rating-component.github.io/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "86efc877dcb2cd9839067367e63a6001",
+    "url": "/camidesarrollo/Interactive-rating-component.github.io/index.html"
+  }
+];
